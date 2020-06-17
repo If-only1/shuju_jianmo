87cd256e217d2e967c73c477dbb6d0bae33839d7
lixianyang <1052308104@qq.com>
2020-06-17 10:49:56 +0800

    write the code of lunwen

--- luwen/main.py
+++ luwen/main.py
@@ -68,33 +68,13 @@ print('train.shape',test.shape)
 from torch.utils.data.dataset import Dataset
 from torch.utils.data.dataloader import DataLoader
 import torch.nn.functional as F
-
-
-class Mydataset(Dataset):
-    def __init__(self, data, feature_names, transform=None):
-        super().__init__()
-        self.data = data
-        self.feature_names = feature_names
-        self.tansform = transform
-        if self.tansform:
-            self.data = self.tansform(data).squeeze(0)
-
-    def __getitem__(self, index):
-        return self.data[index]
-
-    def __len__(self):
-        return self.data.shape[0]
-
-
-
-
 import torch
 import torchvision
 import torch.nn as nn
 from torch.utils.data.dataset import TensorDataset
 
-train_data_tensor = torch.Tensor(train_data)
-test_data_tensor = torch.Tensor(test_data)
+train_data_tensor = torch.Tensor(train)
+test_data_tensor = torch.Tensor(test)
 print('train_data_tensor.size(),test_data_tensor.size(),',
       train_data_tensor.size(), test_data_tensor.size(), )
 # train_dataset = Mydataset(train_data, feature_names, torchvision.transforms.ToTensor())
@@ -103,55 +83,35 @@ train_dataloader = DataLoader(train_dataset, batch_size=64, shuffle=True, drop_l
 
 
 import torch
-
-class TwoLayerNet(torch.nn.Module):
-    def __init__(self, D_in, H, D_out):
-        """
-        在构造函数中，我们实例化了两个nn.Linear模块，并将它们作为成员变量。
-        """
-        super(TwoLayerNet, self).__init__()
-        self.linear1 = torch.nn.Linear(D_in, H)
-        self.linear2 = torch.nn.Linear(H, D_out)
-
-    def forward(self, x):
-        """
-        在前向传播的函数中，我们接收一个输入的张量，也必须返回一个输出张量。
-        我们可以使用构造函数中定义的模块以及张量上的任意的（可微分的）操作。
-        """
-        h_relu = self.linear1(x).clamp(min=0)
-        y_pred = self.linear2(h_relu)
-        return y_pred
-
-# N是批大小； D_in 是输入维度；
-# H 是隐藏层维度； D_out 是输出维度
-N, D_in, H, D_out = 64, 13, 100, 1
-
-
-
+import luwen.mymodel as mod
 # 通过实例化上面定义的类来构建我们的模型。
-model = TwoLayerNet(D_in, H, D_out)
+model = mod.Mlp(100)
 
 # 构造损失函数和优化器。
 # SGD构造函数中对model.parameters()的调用，
 # 将包含模型的一部分，即两个nn.Linear模块的可学习参数。
-loss_fn = torch.nn.MSELoss(reduction='mean')
 optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
-for t in range(200):
-    # 前向传播：通过向模型传递x计算预测值y
-    for data in train_dataloader:
-        data=data[0]
-        x=data[:,:-1]
-        y=data[:,-1:]
-        y_pred = model(x)
+loss_fn = torch.nn.MSELoss(reduction='mean')
 
-        #计算并输出loss
-        loss = loss_fn(y_pred, y)
-        print(t, loss.item())
 
-        # 清零梯度，反向传播，更新权重
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
+def train(model,train_dataloader,loss_fn,optimizer):
+    for t in range(200):
+        # 前向传播：通过向模型传递x计算预测值y
+        for data in train_dataloader:
+            data=data[0]
+            x=data[:,:-1]
+            y=data[:,-1:]
+            y_pred = model(x)
+            #计算并输出loss
+            loss = loss_fn(y_pred, y)
+            print(t, loss.item())
+            # 清零梯度，反向传播，更新权重
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+
+
 pre=model(test_data_tensor[:,:-1]).detach().numpy()
 print(pre)
 
